2eaf8e942866689b2302eab47d45883e927595ce
alex ivory <ivoryaj@gmail.com>
2015-08-06 23:18:54 -0600

    4th

--- koachee/settings.py
+++ koachee/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'koachee.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(MAIN_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
