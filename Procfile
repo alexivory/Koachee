e1eeee9d93a4165e3d6a4cef31ae52f8b2f061ad
alex ivory <ivoryaj@gmail.com>
2015-08-06 23:14:00 -0600

    third

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn coffeed.wsgi --log-file -
+web: gunicorn koachee.wsgi --log-file -
--- koachee/wsgi.py
+++ koachee/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+application = Cling(get_wsgi_application())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "koachee.settings")
 
