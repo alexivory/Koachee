2b9902750ae1094eca420111a5509660f423235e
alex ivory <ivoryaj@gmail.com>
2015-08-06 23:30:46 -0600

    5th

--- koachee/settings.py
+++ koachee/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(file))
+MAIN_DIR = os.path.dirname(os.path.dirname(file))
 
 
 # Quick-start development settings - unsuitable for production
